4b323de810c0b1ae597c68c423d48f1605fd8ce9
Anton Anufriev <anufriev@gmail.com>
2018-04-12 00:43:00 +1000

    add onComplete to RegionMapperFactory

--- raft-mmap/src/main/java/org/dev4fx/raft/mmap/impl/RegionFactory.java
+++ raft-mmap/src/main/java/org/dev4fx/raft/mmap/impl/RegionFactory.java
@@ -32,10 +32,21 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 public interface RegionFactory<T extends Region> {
-    RegionFactory<AsyncRegion> ASYNC_ATOMIC_STATE_MACHINE = (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncAtomicStateMachineRegion(fileChannelSupplier, Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size, 2, TimeUnit.SECONDS);
-    RegionFactory<AsyncRegion> ASYNC_VOLATILE_STATE_MACHINE = (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncVolatileStateMachineRegion(fileChannelSupplier, Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size, 2, TimeUnit.SECONDS);
-    RegionFactory<AsyncRegion> ASYNC_ATOMIC_EXCHANGE = (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncAtomicExchangeRegion(fileChannelSupplier, Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size, 2, TimeUnit.SECONDS);
-    RegionFactory<Region> SYNC = (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new SyncRegion(fileChannelSupplier, Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size);
+    RegionFactory<AsyncRegion> ASYNC_ATOMIC_STATE_MACHINE =
+            (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncAtomicStateMachineRegion(fileChannelSupplier,
+                    Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size,
+                    2, TimeUnit.SECONDS);
+    RegionFactory<AsyncRegion> ASYNC_VOLATILE_STATE_MACHINE =
+            (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncVolatileStateMachineRegion(fileChannelSupplier,
+                    Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size,
+                    2, TimeUnit.SECONDS);
+    RegionFactory<AsyncRegion> ASYNC_ATOMIC_EXCHANGE =
+            (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new AsyncAtomicExchangeRegion(fileChannelSupplier,
+                    Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size,
+                    2, TimeUnit.SECONDS);
+    RegionFactory<Region> SYNC =
+            (size, fileChannelSupplier, fileSizeEnsurer, mapMode) -> new SyncRegion(fileChannelSupplier,
+                    Region.IoMapper.DEFAULT, Region.IoUnMapper.DEFAULT, fileSizeEnsurer, mapMode, size);
 
     T create(int size,
              Supplier<FileChannel> fileChannelSupplier,
